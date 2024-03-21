791d4b87f16a1481ee5c00b63c57a76bf6dda524
119kanika <kanika.mahindroo@gmail.com>
2024-03-21 20:49:49 +0530

    tinymce api

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -14,7 +14,7 @@ export default function RTE({name, control, label, defaultValue =""}) {
     control={control}
     render={({field: {onChange}}) => (
         <Editor
-        apiKey= {conf.apiKey}
+        apiKey='cufras3dc34kr692tkliibyulcnrjil4dvsux9uuhl2arnr3'
         initialValue={defaultValue}
         init={{
             initialValue: defaultValue,
